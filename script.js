7a93fd902a08cbfc18cc660cc1b69879485a82ad
Joshua Taylor <toasterrage@gmail.com>
2022-10-19 17:21:45 -0600

    Updated JS with generatePassword(), password length code, and length validation

--- script.js
+++ script.js
@@ -18,10 +18,35 @@ WHEN the password is generated
 THEN the password is either displayed in an alert or written to the page
 */
 
-/* TO DO - Write generatePassword() function that returns a password 
-fitting the selected criteria*/
+/* Function that returns a password fitting the user selected criteria
+and validates that the minimum acceptable was selected*/
 
-// TO DO - Use prompt() method to get user input on the length of the password
+const generatePassword = () => {
+  // local scope variables used in this function
+  let pwLength = 0;
+
+  // Loop to prompt and validate user input on password length
+  while (pwLength < 8) {
+    // Use prompt() method to get user input on the length of the password
+    pwLength = prompt("Please enter the password length. It must be between 8 and 128 characters long.", 8);
+    // convert pwLength from type string to number
+    pwLength = Number(pwLength)
+
+    // Validate input is acceptable by security criteria
+    if (isNaN(pwLength) || pwLength < 8) {
+      //Use the alert() method to inform the user of invalid input
+      alert("ERROR! Invalid password length. A number of 8 or higher must be entered.");
+      pwLength = 0;
+    }
+  };
+
+
+
+
+
+
+  return "password-here" // TO DO - add completed password
+}
 
 // TO DO - Validate input is acceptable by security criteria
 // TO DO - Use the alert() method to inform the user of invalid input
